c9c54b5e1f2502fb8e24f164e2fccd3664bf649e
yuriy-aza <yuriy.aza@gmail.com>
2023-05-05 17:24:14 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,21 +1,41 @@
 import axios from 'axios';
 
-export class BookAPI {
-  // #BASE_URL = 'https://books-backend.p.goit.global/books/';
+export class Api {
+  #SOURCE_URL = 'https://pixabay.com/api/';
+  #API_KEY = '35549464-6e431a9fbd4d75e0b6b25f5be';
 
-  async getCategoryList() {
-    // try {
-    const response = await axios.get(
-      'https://books-backend.p.goit.global/books/category-list',
-      {
-        params: {},
-      }
-    );
-    console.log('cat-list api');
-    console.log(response);
-    return response.data;
-    // } catch (error) {
-    console.log(error);
-    // } 
+  constructor() {
+    this.lastSearch = '';
+    this.perPage = 40;
+    this.currentPage = 1;
+    this.isNewSearch = false;
+    this.isEndOfPages = false;
+  }
+
+  async getData(query) {
+    if (query !== this.lastSearch) {
+      this.isNewSearch = true;
+      this.currentPage = 1;
+    } else {
+      this.isNewSearch = false;
+    }
+
+    const response = await axios.get(this.#SOURCE_URL, {
+      params: {
+        key: this.#API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: this.perPage,
+        page: this.currentPage,
+      },
+    });
+
+    this.lastSearch = query;
+    this.isEndOfPages =
+      this.perPage * this.currentPage >= response.data.totalHits;
+    this.currentPage += 1;
+    return response;
   }
 }
