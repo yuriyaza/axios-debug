6257da97d64e0e5d0111d07a5c9dec93578234ef
yuriy-aza <yuriy.aza@gmail.com>
2023-05-06 21:49:12 +0300

    Self Run 2

--- src/js/api.js
+++ src/js/api.js
@@ -1,60 +1,69 @@
 import axios from 'axios';
 
 export class BookAPI {
-    // #BASE_URL = 'https://books-backend.p.goit.global/books/';
+  // #BASE_URL = 'https://books-backend.p.goit.global/books/';
 
-async getCategoryList(){
-    
+  async getCategoryList() {
     try {
-        const response = await axios.get('https://books-backend.p.goit.global/books/category-list');
-        console.log('cat-list api');
-        console.log(response.data);
-        return response.data;
+      const response = await axios.get(
+        'https://books-backend.p.goit.global/books/category-list'
+      );
+      console.log('cat-list api');
+      console.log(response.data);
+      return response.data;
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-    };
+  }
 
-    async getTopBooks() {
-        try {
-            const response = await axios.get('https://books-backend.p.goit.global/books/top-books');
-            console.log('top books api');
-            console.log(response.data);
-            return response.data;
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  async getTopBooks() {
+    try {
+      const response = await axios.get(
+        'https://books-backend.p.goit.global/books/top-books'
+      );
+      console.log('top books api');
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    async getCategory(data) {
-        const category = data;
-        console.log(category);
-        try {
-            const response = await axios.get(`https://books-backend.p.goit.global/books/category?category=Audio%20Fiction`);
-            console.log('category api');
-            console.log(response.data);
-            return response.data;
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  async getCategory(data) {
+    const category = data;
+    console.log(category);
+    try {
+      const response = await axios.get(
+        `https://books-backend.p.goit.global/books/category?category=Audio%20Fiction`
+      );
+      console.log('category api');
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    async getBookById(data) {
-        const id = data;
-        try {            
-            const response = await axios.get(`https://books-backend.p.goit.global/books/${id}`);
-            console.log('book api');
-            console.log(response.data);
-            return response.data;
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    
-};
+  async getBookById(data) {
+    const id = data;
+    try {
+      const response = await axios.get(
+        `https://books-backend.p.goit.global/books/${id}`
+      );
+      console.log('book api');
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
-(function fn1() { 
-    console.log('Викликаю сама себе');
+(function fn1() {
+  console.log('Викликаю сама себе');
 })();
 
-
+(async function fn2() {
+    await axios.get(`https://books-backend.p.goit.global/books/category?category=Audio%20Fiction`);
+    console.log('Викликаю сама себе асинхронно');
+})();
